b70dd235b4a2101d5333bff3a7a429a829d52f3a
Richard PC <rifernun@gmail.com>
2025-07-09 17:24:59 -0300

    feat: audio recording and question creation

--- src/pages/create-room.tsx
+++ src/pages/create-room.tsx
@@ -1,28 +1,14 @@
-import { useQuery } from '@tanstack/react-query'
-import  { Link } from 'react-router-dom'
-
-type GetRoomsAPIReponse = Array<{
-  id: string
-  name: string
-}>
+import { CreateRoomForm } from "@/components/create-room-form";
+import { RoomList } from "@/components/room-list";
 
 export function CreateRoom(){
-  const { data, isLoading } = useQuery({
-    queryKey: ['get-rooms'],
-    queryFn: async () => {
-      const response = await fetch('http://localhost:3333/rooms')
-      const result: GetRoomsAPIReponse = await response.json()
-      return result
-    }
-  })
   return (
-    <div>
-
-      {isLoading && <p>Carregando...</p>}
-      <div className="flex flex-col gap-1">
-        {data?.map((room) => {
-          return <Link key={room.id} to={`/room/${room.id}`}>{room.name}</Link>
-        })}
+    <div className='min-h-screen px-4 py-8'>
+      <div className='mx-auto max-w-4xl'>
+        <div className="grid gap-8 grid-cols-2 items-start">
+          <CreateRoomForm/>
+          <RoomList/>
+        </div>
       </div>
     </div>
   )
---
+++ src/pages/record-room-audio.tsx
@@ -0,0 +1,113 @@
+/** biome-ignore-all lint/suspicious/noConsole: <explanation> */
+import { Button } from "@/components/ui/button";
+import { useRef, useState } from "react";
+import { Navigate, useParams } from "react-router-dom";
+
+
+const isRecordingSupported = !!navigator.mediaDevices 
+&& typeof navigator.mediaDevices.getUserMedia === 'function'
+&& typeof window.MediaRecorder === 'function'
+
+type RoomParams = {
+  roomId: string
+}
+
+export function RecordRoomAudio(){
+    const params = useParams<RoomParams>()
+    const [isRecording, setIsRecording] = useState(false)
+    const recorder = useRef<MediaRecorder | null>(null)
+    const intervalRef = useRef<NodeJS.TimeOut>(null)
+
+    function stopRecording(){
+        setIsRecording(false)
+        if(recorder.current && recorder.current.state !== 'inactive'){
+            recorder.current.stop()
+        }
+
+        if(intervalRef.current){
+            clearInterval(intervalRef.current)
+        }
+    }
+
+    async function uploadAudio(audio: Blob){
+        const formData = new FormData()
+
+        formData.append('file', audio, 'audio.webm')
+
+        const response = await fetch(`http://localhost:3333/rooms/${params.roomId}/audio`, {
+            method: 'POST',
+            body: formData,
+        })
+
+        const result = await response.json()
+
+        console.log(result)
+    }
+
+    async function startRecording(){
+        
+        if(!isRecordingSupported){
+            alert('Seu navegador nao suporta gravação!')
+            return
+        }
+
+        setIsRecording(true)
+
+        const audio = await navigator.mediaDevices.getUserMedia({
+            audio: {
+                echoCancellation: true,
+                noiseSuppression: true,
+                sampleRate: 44_100,
+            }
+        })
+
+        createRecorder(audio)
+
+       intervalRef.current = setInterval(()=>{
+        recorder.current?.stop()
+
+        createRecorder(audio)
+       }, 5000)
+    }
+
+
+    function createRecorder(audio: MediaStream){
+        recorder.current = new MediaRecorder(audio, {
+            mimeType: 'audio/webm',
+            audioBitsPerSecond: 64_100,
+        })
+        
+        recorder.current.ondataavailable = event => {
+            if(event.data.size > 0){
+                uploadAudio(event.data)
+            }
+        }
+
+        recorder.current.onstart = () => {
+            console.log('Gravacao iniciada ')
+        }
+
+        recorder.current.onstop = () => {
+            console.log('Gravacao encerrada/pausada')
+        }
+
+        recorder.current.start()
+    }
+    
+    if (!params.roomId) {
+        return <Navigate replace to="/" />
+    }
+
+
+
+    return(
+        <div className="flex h-screen flex-col items-center justify-center gap-3">
+            {isRecording ? (
+                <Button onClick={stopRecording}>Pausar áudio</Button>
+            ) : (
+                <Button onClick={startRecording}>Gravar áudio</Button>
+            )}
+            {isRecording ? <p>Gravando...</p> : <p>Pausado</p>}
+        </div>
+    )
+}
